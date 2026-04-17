59528590423e322aa7906379ae200f3155ec3b42
victo <victorcordeirofp@gmail.com>
2026-04-17 20:33:36 -0300

    feat: create database

---
+++ my-project-fastapi/database/sql_connect.py
@@ -0,0 +1,20 @@
+from fastapi.params import Depends
+from sqlalchemy import create_engine
+from sqlalchemy.sql.annotation import Annotated
+from sqlmodel import SQLModel, Session
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False, "timeout": 10}
+
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
