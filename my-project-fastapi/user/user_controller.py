09b4f1c745fe062f8060838ac63dddbbd6f3ca7c
victo <victorcordeirofp@gmail.com>
2026-04-18 16:57:48 -0300

    feat: Controller operacoes p/ user data

---
+++ my-project-fastapi/user/user_controller.py
@@ -0,0 +1,29 @@
+from typing import Any, Coroutine
+
+from fastapi import Depends
+from typing import Annotated
+
+from database.sql_connect import SessionDep
+from user.user_repository import UserRepository
+from user.user_service import UserService
+from user.user_table import UserTable
+
+
+
+class UserController:
+    def __init__(self, service: UserService):
+        self.service = service
+
+    def create_controller(self, user: UserTable) -> UserTable:
+        result = self.service.create_new_user(user)
+        return result
+
+    async def get_user_controller(self, user_email: str) -> bool | None:
+        return self.service.get_by_email(user_email)
+
+def get_user_controller(db: SessionDep):
+    repo = UserRepository(db)
+    service = UserService(repo)
+    return UserController(service)
+
+UserControllerDep = Annotated[UserController, Depends(get_user_controller)]
