4dafa4bae98aa67e9e973da0419b818d11f36929
victo <victorcordeirofp@gmail.com>
2026-04-18 16:58:23 -0300

    feat: rota p/ funcionalidades user

--- my-project-fastapi/main.py
+++ my-project-fastapi/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from client.client_router import router_client
 from database.sql_connect import create_db_and_tables
+from user.user_router import router_user
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -10,6 +12,5 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-
-
 app.include_router(router_client)
+app.include_router(router_user)
