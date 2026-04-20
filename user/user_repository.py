8c702ed31f2681c7b88ebbfc1168fe0d56f4c84b
victo <victorcordeirofp@gmail.com>
2026-04-20 19:21:40 -0300

    refactor: uso do UserReduce

--- user/user_repository.py
+++ user/user_repository.py
@@ -4,22 +4,23 @@ from sqlalchemy.dialects.sqlite import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import Session, select
 
-from user.user_table import UserTable
+from user.user_table import UserTable, UserReduce
+
 
 class UserRepository:
     def __init__(self, db_table: AsyncSession):
         self.db_table = db_table
 
-    async def create(self, user: UserTable) -> UserTable:
+    async def create(self, user: UserTable):
         self.db_table.add(user)
         await self.db_table.commit()
         await self.db_table.refresh(user)
-        return user
+        return UserReduce(id=user.id, email=user.email, nome=user.nome, role=user.role)
 
     async def get_by_email(self, user_email: str):
         result = await self.db_table.execute(select(UserTable).where(UserTable.email == user_email))
         return result.scalar_one_or_none()
 
     async def get_by_id(self, user_id: int):
-        result = await self.db_table.execute(select(UserTable).where(UserTable.id == user_id))
+        result = await self.db_table.execute(select(UserReduce).where(UserTable.id == user_id))
         return result.scalar_one_or_none()
