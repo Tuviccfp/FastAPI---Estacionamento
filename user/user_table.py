77b0e03eb1765cdb4b8653b999ac356986be2728
victo <victorcordeirofp@gmail.com>
2026-04-20 19:18:19 -0300

    feat: modelo de usuário reduzido

--- user/user_table.py
+++ user/user_table.py
@@ -6,6 +6,13 @@ from sqlmodel import SQLModel, Field
 class UserEmailDTO(SQLModel):
     email: str
 
+@dataclass
+class UserReduce(SQLModel):
+    id: int | None = Field(default=None, primary_key=True)
+    nome: str = Field(index=True, nullable=False)
+    email: str = Field(index=True, nullable=False)
+    role: str = Field(default="basic", nullable=False, index=True)
+
 class UserTable(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     nome: str = Field(index=True, nullable=False)
