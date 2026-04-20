51e7405b1d0942800c9c9355eec7409cfe056216
victo <victorcordeirofp@gmail.com>
2026-04-20 19:21:16 -0300

    refactor: HTTPException nos casos de auth user, login service, uso do UserReduce

--- user/user_service.py
+++ user/user_service.py
@@ -1,11 +1,11 @@
+from datetime import timedelta
 from fastapi import HTTPException
-from pwdlib import PasswordHash
 from pygments.lexers import pascal
 
+from middleware.auth.token import create_access_token, Token
 from user.user_repository import UserRepository
-from user.user_table import UserTable
+from user.user_table import UserTable, UserReduce
 from utils.hash import verify_password, create_hash
-
 
 class UserService:
     def __init__(self, user_repo: UserRepository):
@@ -14,28 +14,36 @@ class UserService:
     async def __get_by_email(self, email: str):
         return await self.user_repo.get_by_email(email)
 
-    async def __authenticate_user(self, email: str, password: str):
+    async def __authenticate_user(self, email: str, password: str) -> UserTable:
         user = await self.__get_by_email(email)
         if not user:
-            verify_password(password, user.password)
-            return False
+            raise HTTPException(status_code=404, detail="Usuário inexistente")
         if not verify_password(password, user.password):
-            return False
+            raise HTTPException(status_code=401, detail="Usuário ou senha inválidos")
         return user
 
     async def create_new_user(self, user: UserTable):
-        get_email = self.__get_by_email(user.email)
-        if await get_email:
+        get_email = await self.__get_by_email(user.email)
+        if get_email:
             raise HTTPException(status_code=409, detail="E-mail já existe")
         new_user: UserTable = UserTable(
             nome=user.nome,
             email=user.email,
             password=create_hash(user.password),
+            role=user.role
         )
-        return await self.user_repo.create(new_user)
+        await self.user_repo.create(new_user)
+        return UserReduce(id=new_user.id, email=new_user.email, nome=new_user.nome, role=new_user.role)
 
     async def get_by_id(self, user_id: int):
-        return await self.user_repo.get_by_id(user_id)
+        user = await self.get_by_id(user_id)
+        return UserReduce(id=user.id, email=user.email, nome=user.nome, role=user.role)
 
-    # async def user_login(self, ):
+    async def user_login(self, email: str, password: str):
+        user = await self.__authenticate_user(email, password)
+        if not user:
+            raise HTTPException(status_code=401, detail="")
+        acess_expires_token = timedelta(minutes=30)
+        create_token = create_access_token(data={"id": user.id, "role": user.role}, expires_delta=acess_expires_token)
+        return Token(acess_token=create_token, token_type="bearer")
 
